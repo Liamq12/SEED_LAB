1cd36e9e2f6acd232b480979cc8f5016b51661b6
Andrew Larkworthy <andrew@larkworthy.org>
2024-09-22 10:42:49 -0600

    Added code documentation in miniproject/main.py

--- MiniProject/main.py
+++ MiniProject/main.py
@@ -9,6 +9,7 @@ from smbus2 import SMBus
 import serial
 from multiprocessing import Process, Pipe
 
+#LCD I2C Connection Handler
 def lcdHandler(lcdConn):
     lcd_columns=16
     lcd_rows = 2
@@ -19,49 +20,40 @@ def lcdHandler(lcdConn):
     lcd.message="Online"
     previous = None
     while True:
+        #Recieves move commands from main process
         toPrint = lcdConn.recv()
         if previous != toPrint:
             previous = toPrint
             lcd.message = f"[{toPrint[0]},{toPrint[1]}]"
         
-
+#Arduino Serial Connection Handler
+#Responsible for sending data to Arduino
 def serialHandler(serialConn):
-    # I2C address of Arduino, set in Arduino sketch
-    #ARD_ADDR=8
-    #Offset register
-    #offset=2
-    #Initialize SMbus library with I2C bus 1
-    #i2c = SMBus(1)
+    #Initializes Serial Connection
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout =1)
     prePosition = None
     
     while True:
+        #Recieves move commands from main process
         messageObj = serialConn.recv()
         leftWheel = messageObj[0]
         rightWheel = messageObj[1]
         toSend = f"[{leftWheel},{rightWheel}]"
-        #command = [ord(character) for character in toSend]
         if prePosition != messageObj:
             ser.write(toSend.encode(encoding='ascii'))
             prePosition = messageObj
-            #try:
-                #i2c.write_i2c_block_data(ARD_ADDR,offset,command)
-                
-           # except IOError:
-           #     print(f"Could not write data to Arduino, offset register: {offset}")
-           # except ValueError:
-           #     print("Message must be 32 bytes or less")
-        
         
 if __name__ == '__main__':
     
-    # Set up pipe
+    # Sets up pipes for interprocess communication
     serialConn,mainSerialConn = Pipe()
     lcdConn,mainLCDConn = Pipe()
     
-    
+    #Defines Arduino Serial Process
     serial_process = Process(target = serialHandler, args=(serialConn,))
+    #Defines LCD iC2 Process
     lcd_process = Process(target = lcdHandler, args=(lcdConn,))
+    #Starts Processes
     serial_process.start()
     lcd_process.start()
     
@@ -72,13 +64,11 @@ if __name__ == '__main__':
     #Arcuo setup
 
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
-
-    ##lcd.message = "Online"
-
-    #id to use for movement
-    #idSelected =
     #Let camera warmup
     sleep(1)
+
+    #Main control loop
+    #Takes image frame from video capture and processes it
     while camera.isOpened():
         ret, frame = camera.read()
         heightCam, widthCam, _ = frame.shape
@@ -86,11 +76,14 @@ if __name__ == '__main__':
         corners,ids,rejected = aruco.detectMarkers(grey,aruco_dict)
         overlay = cv2.cvtColor(grey,cv2.COLOR_GRAY2RGB)
         overlay = aruco.drawDetectedMarkers(overlay,corners,borderColor=4)
+        
+        #If Marker is detected, perform advanced image processing
         if ids is not None:
             ids = ids.flatten()
             centerX = round((corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0])*.25)
             centerY = round((corners[0][0][0][1] + corners[0][0][1][1] + corners[0][0][2][1] + corners[0][0][3][1])*.25)
-            cv2.line(overlay,pt1=(centerX,centerY), pt2=(centerX,centerY), color = (0,0,255), thickness=10)
+            #Debug code below, uncomment to see where cv sees the center
+            #cv2.line(overlay,pt1=(centerX,centerY), pt2=(centerX,centerY), color = (0,0,255), thickness=10)
             leftW = -1
             rightW = -1
             #if true, centerX is in West
@@ -117,36 +110,43 @@ if __name__ == '__main__':
                 rightW = 0
             
             
-            #Sending Process
+            #Sending Data to Sub-Processes
             messageToSend = [0,0]
             messageToSend[0] = leftW
             messageToSend[1] = rightW 
             mainSerialConn.send(messageToSend)
             mainLCDConn.send(messageToSend)
             
+            #Labels marker with numeric id
             for(outline, id) in zip(corners, ids):
                 markerCorners = outline.reshape((4,2))
                 overlay = cv2.putText(overlay,str(id),(int(markerCorners[0,0]),int(markerCorners[0,1]) -15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
         
+        #Defines horizontal and vertical line thickness and color
         thicknessLine = 2
+        #color=(blue,green,red)
         colorLine = (0,0,255)
-        #Horizontal
+        #Horizontal Line
         cv2.line(overlay,pt1=(0,round(heightCam/2)), pt2=(widthCam,round(heightCam/2)), color = colorLine, thickness=thicknessLine)
         
-        #Vertical
+        #Vertical Line
         cv2.line(overlay,pt1=(round(widthCam/2),0), pt2=(round(widthCam/2),heightCam), color = colorLine, thickness=thicknessLine)
-        #color=(blue,green,red)
+        
         cv2.imshow("overlay",overlay)
         
+        #Exits loop after pressign 'q'
         if cv2.waitKey(10) == ord('q'):
             break
-            
+    
+    #Releases camera and kills all windows
     camera.release()
     cv2.destroyAllWindows()
     
     
-    #End multiprocessing
+    #Closes pipes and joins sub-processes back to main process
     mainSerialConn.close()
+    mainLCDConn.close()
     serial_process.join()
+    lcd_process.join() 
     lcd.clear()
     
