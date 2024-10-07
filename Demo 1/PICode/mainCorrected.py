d05338a30e1042b685ef1d4a407ca564dcff1609
Andrew Larkworthy <andrew@larkworthy.org>
2024-10-07 13:55:46 -0600

    Modified corrected.py

--- Demo 1/PICode/mainCorrected.py
+++ Demo 1/PICode/mainCorrected.py
@@ -18,7 +18,7 @@ def angleFinder():
     #SQUARES_HORIZONTALLY = 5
     #SQUARE_LENGTH = 0.034290# square side length in pixels
     #MARKER_LENGTH = 0.017145 # marker length in meters
-    MARKER_LENGTH = 0.05
+    #MARKER_LENGTH = 0.05
     #read json path data
     json_file_path = '/home/seedlab/calibration1.json'
     with open(json_file_path, 'r') as file:
@@ -35,8 +35,8 @@ def angleFinder():
     h, w = image.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dst, (h,w), 1, (h,w))
     image = cv2.undistort(image, mtx, dst, None, newcameramtx)
-    all_charuco_ids = []
-    all_charuco_corners = []
+    #all_charuco_ids = []
+    #all_charuco_corners = []
     
     dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
     #board = cv2.aruco.CharucoBoard((SQUARES_VERTICALLY, SQUARES_HORIZONTALLY), SQUARE_LENGTH, MARKER_LENGTH, dictionary)
@@ -129,15 +129,15 @@ def lcdHandler(lcdConn):
 if __name__ == '__main__':
     
     #For 4:3
-    #CAMERA_HFOV = 57.15431399
+    CAMERA_HFOV = 57.15431399
     #For 16:9
     #CAMERA_HFOV = 61.37272481
-    CAMERA_HFOV = 55
+    #CAMERA_HFOV = 55
     ARUCO_DICT = cv2.aruco.DICT_6X6_250
     dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
     params = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(dictionary, params)
-    MARKER_LENGTH = 0.017145 # marker length in pixels
+    #MARKER_LENGTH = 0.017145 # marker length in pixels
     # Sets up pipes for interprocess communication
     lcdConn,mainLCDConn = Pipe()
     #Defines LCD iC2 Process
@@ -181,20 +181,20 @@ if __name__ == '__main__':
             ids = ids.flatten()
             #try:
             index = np.where(ids == 0)[0][0]
-            centerX = ((corners[index][0][0][0] + corners[index][0][1][0] + corners[index][0][2][0] + corners[index][0][3][0])*.25)
+            centerX = round((corners[index][0][0][0] + corners[index][0][1][0] + corners[index][0][2][0] + corners[index][0][3][0])*.25)
             centerY = ((corners[index][0][0][1] + corners[index][0][1][1] + corners[index][0][2][1] + corners[index][0][3][1])*.25)
             cX = mtx[0][2]
             cY = mtx[1][2]
-           
+            
             #angle = np.arctan2((cX-centerX),(cY-centerY)) * 180 / np.pi
             #angle = (CAMERA_HFOV)/2 * (cX-centerX)/cX
-            angle =  (CAMERA_HFOV/w) * (cX-centerX)
+            angle =  ((CAMERA_HFOV) * (cX-centerX))/w
             
             newTime = time.time()
-            if prevAngle == round(angle):
-                if newTime - prevTime > 1:
-                    prevTime = newTime
-                    mainLCDConn.send(f"{angle:.2f}")
+            #if prevAngle == round(angle):
+            if newTime - prevTime > 1:
+                prevTime = newTime
+                mainLCDConn.send(f"{angle:.2f}")
             prevAngle = round(angle)
             centXR = round(centerX)
             centYR = round(centerY)
