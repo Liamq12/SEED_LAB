7fec8a1ce573f01a0c7c90a0c1abdf95c73bb1ed
Andrew Larkworthy <andrew@larkworthy.org>
2024-11-06 13:58:11 -0700

    serial actually works

--- Demo2/PIcode/main.py
+++ Demo2/PIcode/main.py
@@ -5,28 +5,40 @@ import cv2
 from cv2 import aruco
 import board
 import busio
+import serial
 from smbus2 import SMBus
 from multiprocessing import Process, Pipe
 import json
-        
+   
+   #f"{distanceError},{angleError}" #distance in meters, angle in rad
+   #f"{TURNDIRECTION}"
 #Data packet Format
 # Angle,       
 def serialHandler(serialConn):
     #Initializes Serial Connection
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout =1)
+    ser = serial.Serial('/dev/ttyACM0', 115200)
     prePosition = None
-    
+    delay= time.time()
     while True:
         #Recieves move commands from main process
+        delay2 = time.time()
         messageObj = serialConn.recv()
-        leftWheel = messageObj[0]
-        rightWheel = messageObj[1]
-        toSend = f"[{leftWheel},{rightWheel}]"
-        if prePosition != messageObj:
-            ser.write(toSend.encode(encoding='ascii'))
-            prePosition = messageObj
-        
-        
+        if(ser.in_waiting):
+                print(ser.readline())
+        elif(messageObj != f"0"):
+            
+                #distanceError = messageObj[0]
+                #angleError = messageObj[1]
+                #turnTo = messageObj[2]
+                
+                #toSend = f"{distanceError},{angleError}"
+            if(delay2-delay > .05): 
+                print(messageObj)
+                delay=delay2 
+                if prePosition != messageObj:
+                     ser.write(messageObj.encode(encoding='ascii'))
+                     prePosition = messageObj
+    
 if __name__ == '__main__':
    
 
@@ -38,6 +50,7 @@ if __name__ == '__main__':
     #For 16:9
     #CAMERA_HFOV = 61.37272481
     #Compensate lost degrees
+    fudgeFactor = 0.0
     CAMERA_HFOV = 51.5
     ARUCO_DICT = cv2.aruco.DICT_6X6_250
     dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
@@ -55,38 +68,36 @@ if __name__ == '__main__':
         json_data = json.load(file)
     mtx = np.array(json_data['mtx'])
     dst = np.array(json_data['dist'])
-    
-    
     # initialize the camera
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(-1)
     #Arcuo setup
     w=640
     h=480
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dst, (w,h), 1, (w,h))
     #newW = roi[2]-roi[0]
     #CAMERA_HFOV = (CAMERA_HFOV/w)*newW
-    
     #aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
     #Let camera warmup
     sleep(1) 
     prevAngle = 370
-    
     prevTime = time.time()
-    
-    
-    
     #Main control loop
     #Takes image frame from video capture and processes it
+    turnTo=2
+    upperGreen = np.array([80, 180, 180])
+    lowerGreen = np.array([45,80,80])
+    upperRed = np.array([15, 255, 255])
+    lowerRed = np.array([0,160,160])
+    upperRed2 = np.array([179, 255, 255])
+    lowerRed2 = np.array([170, 160, 160])
     while camera.isOpened():
 #########################################################################################
+        sleep (.05)
         #Determine if the arrow is pointing left(green) or right(red)
-        
-        
-        
+        messageToSend = None    
         ############################################################################
         angle = None
         feet_dist = None
-        turnTo = 2
         ret, image1 = camera.read()
         image = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
         #h, w = image.shape[:2]
@@ -98,7 +109,7 @@ if __name__ == '__main__':
         corners,ids,rejected = detector.detectMarkers(image)
         #h,w = image.shape[:2]
         #If Marker is detected, perform advanced image processing
-        if ids is not None and not np.where(ids==0)[0].size ==0 :
+        if ids is not None and not np.where(ids==0)[0].size ==0:
             ids = ids.flatten()
             #try:
             index = np.where(ids == 0)[0][0]
@@ -119,10 +130,10 @@ if __name__ == '__main__':
             #distance in feet:
             width = corners[index][0][1][0]-corners[index][0][0][0]
              #print(width)
-            feet_dist = 110 / (width)
+            feet_dist = 0.3048* (110 / width)
             #print(feet_dist)
             ############################################################################
-            if (feet_dist<1.7):
+            if (feet_dist<(1.0*0.3048)):
                 height = corners[index][0][2][1] - corners[index][0][1][1]
                 #For width of mask:
                 #Lower bound
@@ -135,22 +146,15 @@ if __name__ == '__main__':
                 upperYBound =  int(corners[index][0][2][1] + round(0.25*width))
                 upperYBound = upperYBound*(upperYBound < 480) + 480*( upperYBound >= 480)
                 
-                print(f"lowerX {lowerXBound}")
-                print(f"upperX {upperXBound}")
-                print(f"lowerY {lowerYBound}")
-                print(f"upperY {upperYBound}")
+                #print(f"lowerX {lowerXBound}")
+                #print(f"upperX {upperXBound}")
+                #print(f"lowerY {lowerYBound}")
+                #print(f"upperY {upperYBound}")
                 
                 #Height remains the same
                 smallImage = image1[lowerYBound:upperYBound,lowerXBound:upperXBound]
-
                 
                 arrowDetectHSV = cv2.cvtColor(smallImage, cv2.COLOR_BGR2HSV)
-                upperGreen = np.array([80, 180, 180])
-                lowerGreen = np.array([45,80,80])
-                upperRed = np.array([15, 255, 255])
-                lowerRed = np.array([0,160,160])
-                upperRed2 = np.array([179, 255, 255])
-                lowerRed2 = np.array([170, 160, 160])
                 
                 #Display red or green (used to tune allowed array values)
                 maskGreen = cv2.inRange(arrowDetectHSV, lowerGreen, upperGreen)
@@ -168,31 +172,39 @@ if __name__ == '__main__':
                 
                 
                 whichOne = ( resultMtxArray[0] < resultMtxArray[1])
-                print(whichOne)
                 if (resultMtxArray[whichOne] > 100000):
                     if(whichOne):
-                        turnTo = 0
+                        turnTo = 0 # set to max FOV until we see camera
+                        messageToSend = f"L\n"
                         #print("There is a green arrow showing")
                     else:
                         turnTo = 1
+                        messageToSend = f"R\n"
                         #print("There is a red arrow showing")
                         
             angle =  ((CAMERA_HFOV) * (cX-centerX))/w
             
             # Fix issue with negative angles being too large, start at like -0.5 angle
-            if angle > 0.5:
+            if (angle > 0.5):
                 angle = angle + 0.7
-            
-            
+            #if(feet_dist>fudgeFactor):
+            #    fudgeFactor = 0
+            if(turnTo==2):
+                messageToSend=f"{round(feet_dist,3)},{round(angle,3)}\n"
+                mainSerialConn.send(messageToSend)
+            else:
+                mainSerialConn.send(messageToSend)
+                break
             #if prevAngle == round(angle):
         #if smallImage is not None:
         #    cv2.imshow("wow",smallImage)
         
-        
-        mainSerialConn.send([angle,feet_dist,turnTo])
+        #mainSerialConn.send([angle,feet_dist,turnTo])
         #cv2.imshow("overlay",overlay)
         #cv2.imshow("undistort", image)
         #Exits loop after pressign 'q'
+        else:
+            mainSerialConn.send(f"0")
         if cv2.waitKey(10) == ord('q'):
             break
         
